c811a4e5323fb38e8dac704600b18d68ebf995ca
Jan Eitzinger <jan.eitzinger@fau.de>
2021-05-19 15:55:10 +0200

    Fix issue with checkout.

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 rm -rf /var/www/symfony/* /var/www/symfony/.*
-git clone https://github.com/ClusterCockpit/ClusterCockpit.git /var/www/symfony/.
+# git clone https://github.com/ClusterCockpit/ClusterCockpit.git /var/www/symfony/.
+
 cd /var/www/symfony
-git checkout  feature-47-introduce-graphql-api
-composer install --no-dev --optimize-autoloader
+
+git init
+git remote add origin https://github.com/ClusterCockpit/ClusterCockpit.git
+git fetch
+git checkout feature-47-introduce-graphql-api
+composer install --no-dev --no-progress --optimize-autoloader
 yarn install
 yarn encore production
 # bin/console doc:mig:mig --no-interaction
