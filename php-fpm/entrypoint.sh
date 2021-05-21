d29e9aceec508f45f40c464e05f78cf84508fcc9
Jan Eitzinger <jan.eitzinger@fau.de>
2021-05-21 08:08:04 +0200

    Fix errors. Add Symfony entrypoint.

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf /var/www/symfony/* /var/www/symfony/.*
-# git clone https://github.com/ClusterCockpit/ClusterCockpit.git /var/www/symfony/.
-
 cd /var/www/symfony
 
 git init
@@ -12,7 +10,8 @@ git checkout feature-47-introduce-graphql-api
 composer install --no-dev --no-progress --optimize-autoloader
 yarn install
 yarn encore production
-# bin/console doc:mig:mig --no-interaction
-# bin/console doc:fix:load --no-interaction
+# php bin/console doctrine:schema:create  --no-interaction
+#php bin/console doctrine:migrations:migrate --no-interaction
+#php bin/console doc:fix:load --no-interaction
 
 exec "$@"
