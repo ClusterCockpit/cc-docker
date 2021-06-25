507012c80c9b15486d9b68130001770d49265bf1
Lou Knauer <lou.knauer@gmx.de>
2021-06-25 08:50:20 +0200

    Fix suffix of job-archive download

--- data/init.sh
+++ data/init.sh
@@ -2,7 +2,7 @@
 
 mkdir symfony
 wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive.tar.xz
-tar xJf job-archive.tar.bz2
+tar xJf job-archive.tar.xz
 rm ./job-archive.tar.xz
 
 if [ $# -gt 0 ]; then
