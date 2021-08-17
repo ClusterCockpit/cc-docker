255cd59ee62b1054a050cf57f2225dc97d5a6a5a
Jan Eitzinger <jan.treibig@gmail.com>
2021-08-17 18:26:53 +0200

    Change InfluxDB Retention.

--- data/init.sh
+++ data/init.sh
@@ -12,6 +12,7 @@ if [ $# -gt 0 ]; then
     if [ $1 == "dev" ]; then
         # 101 is the uid and gid of the user and group www in the cc-php container running php-fpm.
         # For a demo with no new jobs it is enough to give www read permissions on that directory.
+        echo "This script needs to chown the job-archive directory so that the application can write to it:"
         sudo chown -R 101:101 ./job-archive
 
         mkdir -p influxdb/data
@@ -19,8 +20,8 @@ if [ $# -gt 0 ]; then
         cd influxdb/data
         tar xJf ../../influxdbv2-data.tar.xz
         rm ../../influxdbv2-data.tar.xz
-
         cd ../..
+
         # If development: Use SQL source including INFLUX-Job Metadata instead
         cp ./sql-source/ClusterCockpit-dev.sql ./sql-init/ClusterCockpit.sql
     fi
