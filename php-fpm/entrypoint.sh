67cba6d731367d5c78662e41749b8a94ae5cb194
Christoph Kluge <christoph.kluge@fau.de>
2021-08-18 12:52:57 +0200

    wait for github in php-fpm entrypoint

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 if [ "$APP_CLUSTERCOCKPIT_INIT" = true ]; then
+
+    # Wait for docker dns able to resolve gitub
+    # Solves weird special case of container loading faster than github can be reached
+    until ping -c 1 github.com > /dev/null ; do
+      echo "Could not reach github.com yet ..."
+      sleep 1
+    done
+
     rm -rf /var/www/symfony/* /var/www/symfony/.??*
     git clone https://github.com/ClusterCockpit/ClusterCockpit .
 
