13889ae92da010e24072cd98031d26cbeb8357f4
Jan Eitzinger <jan.treibig@gmail.com>
2021-11-25 09:04:58 +0100

    Change settings for stable support
    * Checkout main branch.
    * Download _stable versions of Fixture data.

--- data/init.sh
+++ data/init.sh
@@ -18,9 +18,9 @@ if [ -d symfony ]; then
 fi
 
 mkdir symfony
-wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive.tar.xz
-tar xJf job-archive.tar.xz
-rm ./job-archive.tar.xz
+wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive_stable.tar.xz
+tar xJf job-archive_stable.tar.xz
+rm ./job-archive_stable.tar.xz
 
 # 101 is the uid and gid of the user and group www-data in the cc-php container running php-fpm.
 # For a demo with no new jobs it is enough to give www read permissions on that directory.
@@ -28,7 +28,7 @@ rm ./job-archive.tar.xz
 # sudo chown -R 82:82 ./job-archive
 
 mkdir -p influxdb/data
-wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/influxdbv2-data.tar.xz
+wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/influxdbv2-data_stable.tar.xz
 cd influxdb/data
-tar xJf ../../influxdbv2-data.tar.xz
-rm ../../influxdbv2-data.tar.xz
+tar xJf ../../influxdbv2-data_stable.tar.xz
+rm ../../influxdbv2-data_stable.tar.xz
