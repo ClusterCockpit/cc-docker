cb420305ab8843dcf9a6aab86d850f1e56fa3dcc
Christoph Kluge <christoph.kluge@fau.de>
2022-03-30 15:54:56 +0200

    Cleanup of php-fpm and related components

--- data/init.sh
+++ data/init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -d symfony ]; then
+if [ -d influxdb ]; then
     echo "Data already initialized!"
     echo -n "Perform a fresh initialisation? [yes to proceed / no to exit]  "
     read -r answer
@@ -16,16 +16,12 @@ if [ -d symfony ]; then
     fi
 fi
 
-mkdir symfony
+# Download example job job-archive
 wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive.tar.xz
 tar xJf job-archive.tar.xz
 rm ./job-archive.tar.xz
 
-# 101 is the uid and gid of the user and group www-data in the cc-php container running php-fpm.
-# For a demo with no new jobs it is enough to give www read permissions on that directory.
-# echo "This script needs to chown the job-archive directory so that the application can write to it:"
-# sudo chown -R 82:82 ./job-archive
-
+# Download data for influxdb2
 mkdir -p influxdb/data
 wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/influxdbv2-data.tar.xz
 cd influxdb/data
